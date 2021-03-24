75972b6bb28d2bc8d65b545bf40078bd73a62178
OllyB <info@ollybolland.com>
2021-03-24 21:15:09 +0000

    fetching data into table and formatting correctly

--- client/src/components/table/index.jsx
+++ client/src/components/table/index.jsx
@@ -1,7 +1,7 @@
 import { Table } from 'reactstrap';
 import './table.styles.scss';
 
-const ResultsTable = ({ studentData, sliderState }) => {
+const ResultsTable = ({ tableData }) => {
 
   // const filteredStudents = (studentData) => {
   //   if (!studentData) return;
@@ -12,24 +12,33 @@ const ResultsTable = ({ studentData, sliderState }) => {
 
   return (
     <Table striped bordered className="mt-4">
+      { tableData &&
       <thead className="dark-head">
         <tr>
-          <th>Photo</th>
-          <th>Name</th>
-          <th>Personal Details</th>
-          <th>Family / Emergancy Contacts</th>
-          <th>SEN</th>
+          <th>{Object.keys(tableData[0])[1]}</th>
+          <th>{Object.keys(tableData[0])[2]}</th>
+          <th>{Object.keys(tableData[0])[3]}</th>
+          <th>{Object.keys(tableData[0])[4]}</th>
+          <th>{Object.keys(tableData[0])[5]}</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
+      }
+      
+        
+          { tableData && tableData.map(obj => {
+            return (
+              <tbody>
+                <tr>
+                <td>{Object.values(obj)[1]}</td>
+                <td>{Object.values(obj)[2]}</td>
+                <td>{Object.values(obj)[3]}</td>
+                <td>{Object.values(obj)[4]}</td>
+                <td>{Object.values(obj)[5]}</td>
+                </tr>
+              </tbody>
+            )
+          })
+          }
     </Table>
   )
 }
--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -1,13 +1,35 @@
+import React, {useState} from 'react';
 import TableOfResults from '../../components/table';
 import {Button, Container} from 'reactstrap'
+import axios from 'axios';
 
 const LandingPage = ({searchResult}) => {
+  const [data, setData] = useState();
+  
+  const handleClick = async(e) => {
+    const apiQuery = e.target.name;
+    const result = await axios.get(`http://localhost:9000/api/${apiQuery}`)
+    setData(result.data)
+  }
+  console.log(data)
+
   return (
   <Container className="pt-4">
-    <Button className="mr-1">Get all Matches</Button>
-    <Button className="mr-1">Get all Players</Button>
-    <Button>Get all Teams</Button>
-    <TableOfResults searchResult={searchResult}/>
+    <Button
+      onClick={handleClick}
+      className="mr-1"
+      name='getMatchData'>Get all Matches
+    </Button>
+    <Button
+      onClick={handleClick}
+      className="mr-1"
+      name='getPlayerData'>Get all Players
+    </Button>
+    <Button
+      onClick={handleClick}
+      name='getTeamData'>Get all Teams
+    </Button>
+    <TableOfResults searchResult={searchResult} tableData={data}/>
   </Container>
   )
 }
